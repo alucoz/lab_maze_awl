3d51a9d03cbcbd8e43c59f6339d35a5d01bdc543
Wiktor Grabarczyk <saber.kh730@gmail.com>
2025-01-08 09:38:18 +0100

    finał 09:37, trzeba dac po polsku wszystko i przetłumaczyc

--- plansza_labirynt/ConsoleApp1/Program.cs
+++ plansza_labirynt/ConsoleApp1/Program.cs
@@ -2,7 +2,7 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[] args) //wywołanie wszystkiego
     {
         Maze maze = new Maze();
         Console.WriteLine("Generated Maze:");
